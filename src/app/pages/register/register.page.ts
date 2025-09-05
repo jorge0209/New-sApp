262903c4f7290c58e51cdfb15f86c46a515cc92d
jorge0209 <jorgemolinagarcia2005@gmail.com>
2025-09-05 14:05:39 -0500

    parte final login y register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,16 +1,104 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { Storage } from 'src/app/shared/services/storage/storage';
+import { IUser } from 'src/app/interfaces/user.interface'; 
+import { CountriesService, Country } from 'src/app/shared/services/countries/countries';
+import * as CryptoJS from 'crypto-js';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public name!: FormControl;
+  public lastName!: FormControl;
+  public country!: FormControl;
+  public email!: FormControl;
+  public password!: FormControl;
+  public confirmPassword!: FormControl;
+  public registerForm!: FormGroup;
 
-  ngOnInit() {
+  public countries: Country[] = [];
+
+  constructor(
+    private readonly storageSrv: Storage,
+    private readonly router: Router,
+    private readonly countriesSrv: CountriesService,
+  ) {
+    this.initForm();
   }
 
+  ngOnInit() {
+    this.countriesSrv.getCountries().subscribe(list => {
+      this.countries = list;
+    });
+  }
+
+  public doRegister() {
+    if (this.registerForm.valid) {
+      let users = this.storageSrv.get<IUser[]>("users");
+      if (!users) {
+        users = [];
+      }
+
+      const exists = users.find(user => user.email == this.email.value);
+      if (exists) throw new Error('That email is already in use');
+
+      const encryptedPassword = CryptoJS.SHA256(this.password.value).toString();
+
+      const newUser: IUser = {
+        ...this.registerForm.value,
+        password: encryptedPassword,
+        confirmPassword: undefined
+      };
+
+      users.push(newUser);
+
+      this.storageSrv.set('users', users);
+      this.registerForm.reset();
+      this.router.navigate(['/login']);
+
+    } else {
+      console.log('Invalid form');
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  private initForm() {
+    this.name = new FormControl('', [Validators.required]);
+    this.lastName = new FormControl('', [Validators.required]);
+    this.country = new FormControl('', [Validators.required]);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [Validators.required]);
+    this.confirmPassword = new FormControl('', [Validators.required]);
+
+    this.registerForm = new FormGroup({
+      name: this.name,
+      lastName: this.lastName,
+      country: this.country,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  private passwordMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  private loadCountries() {
+    this.countriesSrv.getCountries().subscribe((countries) => {
+      this.countries = countries.sort((a: any, b: any) =>
+        a.name.localeCompare(b.name)
+      );
+    });
+
+  }
 }
--- src/app/shared/services/countries/countries.ts
+++ src/app/shared/services/countries/countries.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, shareReplay } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Countries {
-  
+export interface Country {
+  name: string;
+  flag?: string;
+  iso2?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class CountriesService {
+  private readonly url = 'https://countriesnow.space/api/v0.1/countries/flag/unicode';
+  private cache$!: Observable<Country[]>;
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+    if (!this.cache$) {
+      this.cache$ = this.http.get<any>(this.url).pipe(
+        map(resp => {
+          const list = resp?.data ?? resp?.countries ?? resp;
+          if (!Array.isArray(list)) return [];
+          return list
+            .map((it: any) => ({
+              name: it.name ?? it.country ?? it.countryName ?? '',
+              flag: it.unicodeFlag ?? it.flag ?? it.emoji ?? '',
+              iso2: it.iso2 ?? it.countryCode ?? ''
+            }))
+
+            .filter((c: Country) => !!c.name)
+            .sort((a: Country, b: Country) => a.name.localeCompare(b.name));
+        }),
+        catchError(err => {
+          console.error('CountriesService.getCountries error', err);
+          return of([]);
+        }),
+        shareReplay(1)
+      );
+    }
+    return this.cache$;
+  }
 }
